69e883a4b3e1aa95b405cb7ee3e08abbed0de53b
Mark Dittmer <mark.s.dittmer@gmail.com>
2018-12-20 11:37:27 -0500

    WIP: Much simpler scheme

--- api/query/cache/newmon/main.go
+++ api/query/cache/newmon/main.go
@@ -40,67 +40,40 @@ var reqCounter = &counter{
 	m:     &sync.Mutex{},
 }
 
-func manageReqs(mon chan bool, reqBlock chan bool, maxReqs uint) chan int {
-	log.Infof("Request manager started")
+func startMon(maxReqs uint, timeout time.Duration) chan int {
+	log.Infof("Monitor started")
 
 	req := make(chan int)
 	go func() {
-		count := uint(0)
+		var timer chan bool
+		var reqs uint
 		for {
-			if count < maxReqs {
-				select {
-				case n := <-req:
-					log.Infof("Request manager: Accepted request %d", n)
-					count++
-				case <-mon:
-					log.Infof("Request manager: Monitor run complete")
-					count = uint(0)
+			timer = make(chan bool, 1)
+			go func() {
+				time.Sleep(timeout)
+				timer <- true
+			}()
+			select {
+			case <-timer:
+				log.Infof("Monitor: Timed out")
+				time.Sleep(time.Second)
+				reqCounter.reset()
+				time.Sleep(time.Second)
+				reqs = 0
+			case <-req:
+				reqs++
+				if reqs == maxReqs {
+					log.Infof("Monitor: Max reqs")
+					time.Sleep(time.Second)
+					reqCounter.reset()
+					time.Sleep(time.Second)
+					reqs = 0
 				}
-			} else {
-				log.Infof("Request manager: Blocking on request backlog")
-				reqBlock <- true
-				<-mon
-				count = uint(0)
 			}
 		}
 	}()
 
 	return req
-}
-
-func runMon(mon chan bool, reqBlock chan bool, timeout time.Duration) {
-	var timer chan bool
-	for {
-		timer = make(chan bool, 1)
-		go func() {
-			time.Sleep(timeout)
-			timer <- true
-		}()
-		select {
-		case <-timer:
-			log.Infof("Monitor: Timed out")
-			time.Sleep(time.Second)
-			reqCounter.reset()
-			time.Sleep(time.Second)
-		case <-reqBlock:
-			log.Infof("Monitor: Requests blocked on monitor")
-			time.Sleep(time.Second)
-			reqCounter.reset()
-			time.Sleep(time.Second)
-		}
-		log.Infof("Monitor: Monitor run complete")
-		mon <- true
-	}
-}
-
-func startMon(maxReqs uint, timeout time.Duration) chan int {
-	log.Infof("Monitor started")
-
-	mon := make(chan bool)
-	reqBlock := make(chan bool)
-	go runMon(mon, reqBlock, timeout)
-
-	return manageReqs(mon, reqBlock, maxReqs)
 }
 
 func main() {
@@ -115,7 +88,9 @@ func main() {
 			defer wg.Done()
 			r := rand.Intn(20000)
 			time.Sleep(time.Millisecond * time.Duration(r))
+			log.Infof("Starting req %d", j)
 			req <- j
+			log.Infof("Running req %d", j)
 			reqCounter.inc()
 		}(i)
 	}
